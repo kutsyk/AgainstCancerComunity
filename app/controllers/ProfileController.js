2d9e72be29c9f8b680889b63da6d7fadd10b3e42
GeeketteCaro <carolinegreta78@gmail.com>
2017-10-01 13:58:26 +0200

    form added + some shi....

--- app/controllers/ProfileController.js
+++ app/controllers/ProfileController.js
@@ -14,4 +14,16 @@ module.exports = function (app, passport) {
     app.get('/chat', function (req, res) {
         res.render('profile/chat.ejs');
     });
+
+    app.get('/questionnaire', function (req, res) {
+        res.render('profile/questionnaire.ejs');
+    });
+
+    app.get('/prescription', function (req, res) {
+        res.render('profile/prescription.ejs');
+    });
+
+    app.get('/menu2', function (req, res) {
+        res.render('profile/menu2.ejs');
+    });
 };
